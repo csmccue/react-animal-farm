298c7f77ad44a409f1b5f4728515e61a2e84301e
Connor McCue <connor.mccue@comcast.net>
2022-09-14 11:34:41 -0700

    finished product

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -6,7 +6,7 @@ export default function Animal(props) {
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       <p className='name'>{props.name}</p>
       <p>{props.says}</p>
-    </div>;
+    </div>
   </>;
 
 }
